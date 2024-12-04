39f922919cbc822e533c59e5e81b9c9209f03cfa
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2024-12-04 16:39:36 +0100

    Fin paso 6

--- tests/Feature/PageResponseTest.php
+++ tests/Feature/PageResponseTest.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Pest\Laravel\get;
+
+uses(RefreshDatabase::class);
 
 it('gives back succesful respond for home page', function () {
     get(route('home'))->assertOk(); //assert = afirmacion
