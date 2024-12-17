9c50ea63c777d7c75a4591809a8923d7b577d680
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2024-12-17 16:54:52 +0100

    Fin paso 15

--- tests/Feature/Pages/PageDashboardTest.php
+++ tests/Feature/Pages/PageDashboardTest.php
@@ -3,15 +3,13 @@
 use App\Models\Course;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Carbon;
 use function Pest\Laravel\get;
 
-uses(RefreshDatabase::class);
 
 it('cannot be accessed by guest', function () {
     // Act & Assert
-    get(route('dashboard'))
+    get(route('pages.dashboard'))
         ->assertRedirect(route('login'));
 });
 
@@ -26,8 +24,8 @@ it('lists purchased courses', function () {
                 ->create();
 
     // Act & Assert
-    $this->actingAs($user);
-    get(route('dashboard'))
+    loginAsUser($user);
+    get(route('pages.dashboard'))
         ->assertOk()
         ->assertSeeText([
             'Course A',
@@ -37,11 +35,10 @@ it('lists purchased courses', function () {
 
 it('does not list other courses', function () {
     // Arrange
-    $user= User::factory()->create();
     $course = Course::factory()->create();
     // Act
-    $this->actingAs($user);
-    get(route('dashboard'))
+    loginAsUser();
+    get(route('pages.dashboard'))
         ->assertOk()
         ->assertDontSeeText($course->title);
 
@@ -49,7 +46,7 @@ it('does not list other courses', function () {
 
 it('shows latest purchased course first', function () {
     // Arrange
-    $user = User::factory()->create();
+    $user = loginAsUser();
     $firstPurchasedCourse = Course::factory()->create();
     $secondPurchasedCourse = Course::factory()->create();
 
@@ -57,8 +54,7 @@ it('shows latest purchased course first', function () {
     $user->courses()->attach($secondPurchasedCourse, ['created_at' => Carbon::now()]);
 
     // Act
-    $this->actingAs($user);
-    get(route('dashboard'))
+    get(route('pages.dashboard'))
         ->assertOk()
         ->assertSeeTextInOrder([$secondPurchasedCourse->title, $firstPurchasedCourse->title]);
 });
@@ -67,9 +63,9 @@ it('includes link to product videos', function () {
     // Arrange
     $user = User::factory()->has(Course::factory())->create();
     // Act & Assert
-    $this->actingAs($user);
-    get(route('dashboard'))
+    loginAsUser($user);
+    get(route('pages.dashboard'))
         ->assertOk()
         ->assertSeeText('Watch videos')
-        ->assertSee(route('page.course-videos', Course::first()));
+        ->assertSee(route('pages.course-videos', Course::first()));
 });
